20f7d5ffefbfb5942ba6cb0e4702abd767054acf
João Nogueira <joaonogueira@fisica.ufc.br>
2021-07-30 10:17:10 -0300

    validações no form de criação de post

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,7 +1,17 @@
 <h1>Cadastrar Novo Post</h1>
+
+@if ($errors->any())
+    <div>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </div>    
+@endif
+<br>
+
 <form action="{{ route('posts.store') }}" method="post">
     @csrf
-    <input type="text" name="title" id="title" placeholder="Título"><br><br>
-    <textarea name="content" id="content" cols="30" rows="4" placeholder="Conteúdo"></textarea><br><br>
+    <input type="text" name="title" id="title" placeholder="Título" value="{{old('title')}}"><br><br>
+    <textarea name="content" id="content" cols="30" rows="4" placeholder="Conteúdo">{{old('content')}}</textarea><br><br>
     <button type="submit">Criar</button>
 </form>
