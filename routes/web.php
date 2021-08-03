89f3e3fa5a806fad55d2c0514840bc72e40dc675
João Nogueira <joaonogueira@fisica.ufc.br>
2021-08-03 09:41:14 -0300

    breeze authentication started

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,9 @@ Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
