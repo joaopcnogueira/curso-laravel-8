dc4ada8c03355257e9a0439d7029295f04279d93
João Nogueira <joaonogueira@fisica.ufc.br>
2021-08-04 10:04:57 -0300

    autenticação

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::any('/posts/search', [PostController::class, 'search'])->name('posts.search');
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
-Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
-Route::delete('/posts/{id}', [PostController::class, 'delete'])->name('posts.delete');
-Route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
-Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
+Route::middleware(['auth'])->group(function () {
+    Route::any('/posts/search', [PostController::class, 'search'])->name('posts.search');
+    Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
+    Route::delete('/posts/{id}', [PostController::class, 'delete'])->name('posts.delete');
+    Route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
+    Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
+});
 
 
 Route::get('/', function () {
